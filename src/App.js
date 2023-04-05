7399adfece7832c5b355ab4b43331f4dca3342df
Zobeir <rigi.zobir@yahoo.com>
2023-04-05 23:54:52 +0100

    adding search icon and feild

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 import StorefrontIcon from "@mui/icons-material/Storefront";
+import SearchIcon from "@mui/icons-material/Search";
 function App() {
   return (
     <div className="App">
@@ -8,7 +9,10 @@ function App() {
         <StorefrontIcon className="header__logoImage" fontSize="large" />
         <h2 className="header__logoTitle">eShop</h2>
       </div>
-      <div className="header__search"></div>
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
       <div className="haeder__nav">
         <div className="nav__item">
           <span className="nev__itemLineOne">Hello Guest</span>
