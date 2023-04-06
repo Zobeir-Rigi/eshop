e5cbb424fc029e9e6cec3d743a75f3a928ac4387
Zobeir <rigi.zobir@yahoo.com>
2023-04-06 05:16:33 +0100

    updating the pictures sources

--- src/Home.js
+++ src/Home.js
@@ -14,10 +14,10 @@ const Home = () => {
         <div className="home__row">
           <Product
             id="1"
-            title="AKASO Brave 7 Action Camera, IPX8 Waterproof Native 4K 20MP WiFi Cam with Touch Screen EIS 2.0 Zoom Support External Mic Voice Control Vlog Camera"
-            price="159.99"
-            rating="4"
-            image="https://m.media-amazon.com/images/G/02/apparel/rcxgs/tile._CB483369956_.gif"
+            title="Panasonic HC-X20E 4K 60p Professional Camcorder, Video Camera Camcorder with 15-megapixel 1.0-type MOS Sensor, 20x Optical Zoom, i.ZOOM 32x (FHD), 3G-SDI Output, XLR Input, Wired Remote "
+            price="2.181.53"
+            rating="6"
+            image="https://m.media-amazon.com/images/I/71v3aTg8BML._AC_SL1500_.jpg"
           />
           <Product
             id="2"
@@ -30,16 +30,16 @@ const Home = () => {
         <div className="home__row">
           <Product
             id="3"
-            title="DJI Action 2 Dual-Screen Combo WITHOUT Bonus Magnetic Protective Case "
-            price="271.99"
-            rating="5"
-            image="https://m.media-amazon.com/images/G/02/apparel/rcxgs/tile._CB483369956_.gif"
+            title="4K Video Camera Camcorder UHD 48MP WiFi IR Night Vision Vlogging Camera for YouTube Touch Screen 16X Digital Zoom Camera Recorder with Microphone, Handheld Stabilizer, Lens Hood, Remote,2 Batteries  "
+            price="154.99"
+            rating="4"
+            image="https://m.media-amazon.com/images/I/81eUw7H78UL._AC_SL1500_.jpg"
           />
           <Product
             id="4"
             title="GoPro HERO11 Black - Waterproof Action Camera with 5.3K60 Ultra HD Video, 27MP Photos, 1/1.9 Image Sensor, Live Streaming, Webcam, Stabilization "
-            price="11.99"
-            rating="4"
+            price="499.00"
+            rating="5"
             image="https://m.media-amazon.com/images/I/617sGSjmC1L._AC_SL1500_.jpg"
           />
           <Product
