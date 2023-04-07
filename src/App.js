29a7b3ea78100b61339129087bc79840abe54ec5
Zobeir <rigi.zobir@yahoo.com>
2023-04-07 15:06:57 +0100

    Implimenting the routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
 import Home from "./Home";
 import Header from "./header";
+import Checkout from "./Checkout";
+import { BrowserRouter, Routes, Route  } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      {/* <Header />, <Home />, checkout */}
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Checkout" element={<Checkout />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-
   );
 }
 
--- src/header.js
+++ src/header.js
@@ -2,16 +2,19 @@ import React from "react";
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 import StorefrontIcon from "@mui/icons-material/Storefront";
 import SearchIcon from "@mui/icons-material/Search";
-
+import { Link } from "react-router-dom";
 import "./header.css";
 
 const Header = () => {
   return (
     <div className="header">
-      <div className="header__logo">
-        <StorefrontIcon className="header__logoImage" fontSize="large" />
-        <h2 className="header__logoTitle">eShop</h2>
-      </div>
+      <Link to="/" >
+        <div className="header__logo">
+          <StorefrontIcon className="header__logoImage" fontSize="large" />
+          <h2 className="header__logoTitle">eShop</h2>
+        </div>
+      </Link>
+
       <div className="header__search">
         <input type="text" className="header__searchInput" />
         <SearchIcon className="header__searchIcon" />
@@ -25,10 +28,12 @@ const Header = () => {
           <span className="nev__itemLineOne">Your</span>
           <span className="nav__itemLineTwo">Shop</span>
         </div>
-        <div className="nav__itemBasket">
-          <ShoppingBasketIcon />
-          <span className="nav__itemLineTwo nav__basketCount">0</span>
-        </div>
+        <Link to="/Checkout">
+          <div className="nav__itemBasket">
+            <ShoppingBasketIcon />
+            <span className="nav__itemLineTwo nav__basketCount">0</span>
+          </div>
+        </Link>
       </div>
     </div>
   );
