c368d4cc9786dcfd9528aecbae88bfcb5d57b996
Zobeir <rigi.zobir@yahoo.com>
2023-04-08 14:38:21 +0100

    Removing Currency format

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,12 +1,8 @@
 import React from 'react'
-import CurrencyFormat from "react-currency-format";
 import "./Subtotal.css"
 const Subtotal = () => {
   return (
     <div className="subtotal">
-      <CurrencyFormat
-        renderText={(value) => (
-          <>
             <p>
               Subtotal (0 items): <strong>£0</strong>
             </p>
@@ -15,14 +11,6 @@ const Subtotal = () => {
               This order contain a gift
             </small>
             <button>Proceed to Checkout</button>
-          </>
-        )}
-        decimalScale={2}
-        value={0}
-        displayType={"text"}
-        thousandSeparator={true}
-        prefix="£"
-      />
     </div>
   );
 }
