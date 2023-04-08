42f3871e158bfaaa4bc4e60de00d680cc4d0bb9e
Zobeir <rigi.zobir@yahoo.com>
2023-04-08 10:24:29 +0100

    I've installed  react-currency-format and imple it

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import CurrencyFormat from "react-currency-format";
 const Subtotal = () => {
   return (
-    <div>Subtotal</div>
+    <div className='subtotal'>
+      <CurrencyFormat 
+        renderText={(value)=> (
+          <>
+            <p>
+              Subtotal (0 items): <strong>£0</strong>
+            </p>
+            <small className="subtotal__gift">
+              <input type='checkbox'/>This order contain a gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={0}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix='£'
+      />
+    </div>
   )
 }
 
