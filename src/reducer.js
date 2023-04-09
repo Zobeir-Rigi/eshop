72f1f931ed3b9068e412f7db33cf48a895343a68
Zobeir <rigi.zobir@yahoo.com>
2023-04-09 13:35:18 +0100

    added default case to switch

--- src/reducer.js
+++ src/reducer.js
@@ -7,13 +7,15 @@ export const initialState = {
 //Selector
 // current state and action to make changes to th state
 const reducer =(state, action)=>{ // action == dispatch
-    switch (action.type) {
+    switch(action.type) {
       case "ADD_TO_BASKET":
         return {
           ...state,
           basket: [...state.basket, action.item],
-        };
+        }
+         default:
+            return state;
     }
-}
+    }
 
 export default reducer;
