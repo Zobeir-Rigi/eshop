a77219e6f753d90a4e3afb264267beba77d87b30
Zobeir <rigi.zobir@yahoo.com>
2023-04-11 08:24:47 +0100

    updated checkout picture

--- src/Checkout.js
+++ src/Checkout.js
@@ -8,33 +8,32 @@ const Checkout = () => {
     const [{ basket }, dispatch] = useStateValue();
     console.log("I'm dispatch in the Checkout.js",dispatch)
       return (
-    <div className="checkout">
-      <div className="checkout__left">
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnXKKpWFMIcr0OguE6tKEf0GvbJWdG9s_8TIAAbFbqdfCgoJoPRPqN_xudclsDFXpajA&usqp=CAU"
-          alt="adverttisment"
-          className="checkout__ad"
-        />
-     <div className="checkout__title">
-        <h2 >
-        Your shopping Basket</h2>
-        {basket.map((item)=>(
-          <CheckoutProduct 
-            id={item.id}
-            titel={item.title}
-            image={item.image}
-            price={item.price}
-            rating={item.rating}
-          />
-        ))}
-      </div>
-    </div>
-      
-      <div className="Checkout__right">
-        <Subtotal />
-      </div>
-    </div>
-  );
+        <div className="checkout">
+          <div className="checkout__left">
+            <img
+              src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+              alt="adverttisment"
+              className="checkout__ad"
+            />
+            <div className="checkout__title">
+              <h2>Your shopping Basket</h2>
+              {basket.map((item) => (
+                <CheckoutProduct
+                  id={item.id}
+                  titel={item.title}
+                  image={item.image}
+                  price={item.price}
+                  rating={item.rating}
+                />
+              ))}
+            </div>
+          </div>
+
+          <div className="Checkout__right">
+            <Subtotal />
+          </div>
+        </div>
+      );
 }
 
 export default Checkout;
