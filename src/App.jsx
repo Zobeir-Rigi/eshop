901930e0513de805e158415b36fb9fd7954b17f5
zobeir-rigi <rigi.zobir@yahoo.com>
2024-02-19 05:13:27 +0000

    App.css added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Footer from "./components/Footer";
 
+import "./App.css";
 function App() {
   return (
     <div className="App">
